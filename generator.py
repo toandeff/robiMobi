b4de44cb3e50896bc7654548da36aea6cb5eaa5f
unknown <1960717@stud.hs-mannheim.de>
2020-02-01 22:58:34 +0100

    functional model 2d and 3d push with screenshots

--- generator.py
+++ generator.py
@@ -17,7 +17,6 @@ from skimage.transform import resize
 
 import imgaug as ia
 import imgaug.augmenters as iaa
-from imgaug.augmentables.segmaps import SegmentationMapsOnImage
 from imgaug.augmentables.bbs import BoundingBox, BoundingBoxesOnImage
 from imgaug.augmentables import Keypoint, KeypointsOnImage
 
@@ -26,10 +25,6 @@ import os
 import parser as Parser
 
 show_image = True
-
-# define the augmentation
-#seq1 = iaa.Sequential([ iaa.Affine(rotate=(-30,30))]) # Augmentation for images and masks
-#seq2 = iaa.Sequential([ iaa.Dropout([0.1,0.5])])      # Augmentation for images
 
 def visualizeImageAndLabel(image, label, image_aug, label_aug):
 #    image_before = label.draw_on_image(image, size = 2)
@@ -80,7 +75,7 @@ class Generator():
         self.aug_test_imgs = []
         self.aug_test_labels = []        
         
-    def augment_train_data(self):
+    def augment_2D_train_data(self):
         sometimes = lambda aug: iaa.Sometimes(0.5, aug)
         for idx in range (len(self.train_imgs)):
             
@@ -117,10 +112,7 @@ class Generator():
                 )
             ], random_order=True) # apply augmenters in random order
 
-            seq2D.deterministic = True
-            
-#            seq3D = iaa.Sequential([])
-            
+            seq2D.deterministic = True            
 
             aug_img, aug_label = seq2D(image=X,  keypoints=Y)
 
@@ -130,7 +122,7 @@ class Generator():
             self.aug_train_imgs.append(aug_img)
             self.aug_train_labels.append(aug_label)
         
-    def augment_test_data(self):
+    def augment_2D_test_data(self):
         sometimes = lambda aug: iaa.Sometimes(0.5, aug)
         for idx in range (len(self.test_imgs)):
             
@@ -138,9 +130,8 @@ class Generator():
             aug_label = np.zeros(shape=(1))
         
             X = self.test_imgs[idx]
-            Y = KeypointsOnImage([Keypoint(x=self.test_labels[idx][0]*512, y=self.test_labels[idx][1]*512),
-                                      Keypoint(x=self.test_labels[idx][2]*512, y=self.test_labels[idx][3]*512)], shape=X.shape)
-
+            Y = KeypointsOnImage([Keypoint(x=self.train_labels[idx][0]*512, y=self.train_labels[idx][1]*512),
+                                     Keypoint(x=self.train_labels[idx][2]*512, y=self.train_labels[idx][3]*512),], shape=X.shape)
             seq = iaa.Sequential([
                 # Small gaussian blur with random sigma between 0 and 0.5.
                 # But we only blur about 50% of all images.
@@ -167,7 +158,6 @@ class Generator():
 
                 )
             ], random_order=True) # apply augmenters in random order
-
             
             aug_img, aug_label = seq(image=X,  keypoints=Y)
             
@@ -176,13 +166,139 @@ class Generator():
             
             self.aug_test_imgs.append(aug_img)
             self.aug_test_labels.append(aug_label)
-            
-            
-    def get_data_after_augment(self):
 
+    def augment_3D_train_data(self):
+        sometimes = lambda aug: iaa.Sometimes(0.5, aug)
+        for idx in range (len(self.train_imgs)):
+            
+            aug_img = np.zeros(shape=(512,512,3)) 
+            aug_label = np.zeros(shape=(1))
         
-        self.augment_train_data()
-        self.augment_test_data()
+            X = self.train_imgs[idx]
+            Y = KeypointsOnImage([Keypoint(x=self.train_labels[idx][0]*512, y=self.train_labels[idx][1]*512),
+                                      Keypoint(x=self.train_labels[idx][2]*512, y=self.train_labels[idx][3]*512),
+                                      Keypoint(x=self.train_labels[idx][4]*512, y=self.train_labels[idx][5]*512),
+                                      Keypoint(x=self.train_labels[idx][6]*512, y=self.train_labels[idx][7]*512),
+                                      Keypoint(x=self.train_labels[idx][8]*512, y=self.train_labels[idx][9]*512),
+                                      Keypoint(x=self.train_labels[idx][10]*512, y=self.train_labels[idx][11]*512),
+                                      Keypoint(x=self.train_labels[idx][12]*512, y=self.train_labels[idx][13]*512),
+                                      Keypoint(x=self.train_labels[idx][14]*512, y=self.train_labels[idx][15]*512)],
+                                      shape=X.shape)
+
+            seq2D = iaa.Sequential([
+                # Small gaussian blur with random sigma between 0 and 0.5.
+                # But we only blur about 50% of all images.
+                iaa.Sometimes(0.5,
+                    iaa.GaussianBlur(sigma=(0, 0.5))
+                ),
+                # Strengthen or weaken the contrast in each image.
+                iaa.ContrastNormalization((0.75, 1.5)),
+                # Add gaussian noise.
+                # For 50% of all images, we sample the noise once per pixel.
+                # For the other 50% of all images, we sample the noise per pixel AND
+                # channel. This can change the color (not only brightness) of the
+                # pixels.
+                iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5),
+                # Make some images brighter and some darker.
+                # In 20% of all cases, we sample the multiplier once per channel,
+                # which can end up changing the color of the images.
+                iaa.Multiply((0.8, 1.2), per_channel=0.2),
+                # Apply affine transformations to each image.
+                # Scale/zoom them, translate/move them, rotate them and shear them.
+                iaa.Affine(
+                    scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+                    translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+                )
+            ], random_order=True) # apply augmenters in random order
+
+            seq2D.deterministic = True            
+
+            aug_img, aug_label = seq2D(image=X,  keypoints=Y)    
+
+            aug_label = [(aug_label.keypoints[0].x/512), (aug_label.keypoints[0].y/512), 
+                         (aug_label.keypoints[1].x/512), (aug_label.keypoints[1].y/512),
+                         (aug_label.keypoints[2].x/512), (aug_label.keypoints[2].y/512),
+                         (aug_label.keypoints[3].x/512), (aug_label.keypoints[3].y/512),
+                         (aug_label.keypoints[4].x/512), (aug_label.keypoints[4].y/512),
+                         (aug_label.keypoints[5].x/512), (aug_label.keypoints[5].y/512),
+                         (aug_label.keypoints[6].x/512), (aug_label.keypoints[6].y/512),
+                         (aug_label.keypoints[7].x/512), (aug_label.keypoints[7].y/512)]
+     
+            self.aug_train_imgs.append(aug_img)
+            self.aug_train_labels.append(aug_label)
+        
+    def augment_3D_test_data(self):
+        sometimes = lambda aug: iaa.Sometimes(0.5, aug)
+        for idx in range (len(self.test_imgs)):
+            
+            aug_img = np.zeros(shape=(512,512,3)) 
+            aug_label = np.zeros(shape=(1))
+        
+            X = self.test_imgs[idx]
+            Y = KeypointsOnImage([Keypoint(x=self.test_labels[idx][0]*512, y=self.test_labels[idx][1]*512),
+                                      Keypoint(x=self.test_labels[idx][2]*512, y=self.test_labels[idx][3]*512),
+                                      Keypoint(x=self.test_labels[idx][4]*512, y=self.test_labels[idx][5]*512),
+                                      Keypoint(x=self.test_labels[idx][6]*512, y=self.test_labels[idx][7]*512),
+                                      Keypoint(x=self.test_labels[idx][8]*512, y=self.test_labels[idx][9]*512),
+                                      Keypoint(x=self.test_labels[idx][10]*512, y=self.test_labels[idx][11]*512),
+                                      Keypoint(x=self.test_labels[idx][12]*512, y=self.test_labels[idx][13]*512),
+                                      Keypoint(x=self.test_labels[idx][14]*512, y=self.test_labels[idx][15]*512)],
+                                      shape=X.shape)
+            seq = iaa.Sequential([
+                # Small gaussian blur with random sigma between 0 and 0.5.
+                # But we only blur about 50% of all images.
+                iaa.Sometimes(0.5,
+                    iaa.GaussianBlur(sigma=(0, 0.5))
+                ),
+                # Strengthen or weaken the contrast in each image.
+                iaa.ContrastNormalization((0.75, 1.5)),
+                # Add gaussian noise.
+                # For 50% of all images, we sample the noise once per pixel.
+                # For the other 50% of all images, we sample the noise per pixel AND
+                # channel. This can change the color (not only brightness) of the
+                # pixels.
+                iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5),
+                # Make some images brighter and some darker.
+                # In 20% of all cases, we sample the multiplier once per channel,
+                # which can end up changing the color of the images.
+                iaa.Multiply((0.8, 1.2), per_channel=0.2),
+                # Apply affine transformations to each image.
+                # Scale/zoom them, translate/move them, rotate them and shear them.
+                iaa.Affine(
+                    scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+                    translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+
+                )
+            ], random_order=True) # apply augmenters in random order
+            
+            aug_img, aug_label = seq(image=X,  keypoints=Y)
+            
+            aug_label = [(aug_label.keypoints[0].x/512), (aug_label.keypoints[0].y/512), 
+                         (aug_label.keypoints[1].x/512), (aug_label.keypoints[1].y/512),
+                         (aug_label.keypoints[2].x/512), (aug_label.keypoints[2].y/512),
+                         (aug_label.keypoints[3].x/512), (aug_label.keypoints[3].y/512),
+                         (aug_label.keypoints[4].x/512), (aug_label.keypoints[4].y/512),
+                         (aug_label.keypoints[5].x/512), (aug_label.keypoints[5].y/512),
+                         (aug_label.keypoints[6].x/512), (aug_label.keypoints[6].y/512),
+                         (aug_label.keypoints[7].x/512), (aug_label.keypoints[7].y/512)]
+     
+            
+            self.aug_test_imgs.append(aug_img)
+            self.aug_test_labels.append(aug_label)
+            
+            
+    def get_data_after_augment(self, dimensions):
+        
+        if dimensions == '2D':
+            self.augment_2D_train_data()
+            self.augment_2D_test_data()
+          
+        elif dimensions == '3D':
+            self.augment_3D_train_data()
+            self.augment_3D_test_data()
+            
+        else: 
+            return
              
         ret_train_imgs = self.train_imgs
         ret_train_imgs = np.concatenate((ret_train_imgs, np.array(self.aug_train_imgs)), axis=0)
@@ -194,9 +310,6 @@ class Generator():
         ret_test_imgs = np.concatenate((ret_test_imgs, np.array(self.aug_test_imgs)), axis=0)
         
         ret_test_labels = self.test_labels
-#        print("ret_test_labels ",ret_test_labels.shape )
-#        print("self.aug_test_labels ", np.array(self.aug_test_labels).shape)
-#        print("self.aug_test_labels ", self.aug_test_labels)
         ret_test_labels = np.concatenate((ret_test_labels, np.array(self.aug_test_labels)), axis=0)
       
         
@@ -206,37 +319,6 @@ class Generator():
     def drawImageWithKeyPoints(self, idx):
         visualizeImageAndLabel(self.train_imgs[idx], self.train_labels[idx], 
                                self.aug_train_imgs[idx] , self.aug_train_labels[idx])
-
-        
-        
-        
-
-
-    def __getitem__(self, idx):
-        image_augmented = np.zeros(shape=(512,512,3)) 
-        label_augmented = np.zeros(shape=(1))
-
-        X = self.images[idx]
-        
-        Y = BoundingBoxesOnImage([
-            BoundingBox(x1=self.labels[idx][0], y1=self.labels[idx][1], 
-                        x2=self.labels[idx][2], y2=self.labels[idx][3]),], shape=X[idx].shape)
-        self.labels[idx]
-        print("labels values: (%.4f, %.4f, %.4f, %.4f)"%( 
-                self.labels[idx][0], self.labels[idx][1], self.labels[idx][2], self.labels[idx][3]))
-        
-        #iaa.Multiply((1.2, 1.5))
-        #,iaa.Affine(rotate=(-30,30)) 
-
-        seq1 = iaa.Sequential([iaa.Affine(scale=(0.5, 0.7))])
-        #seq2 = iaa.Sequential([iaa.Dropout(p=0.5)])                  
-        seq1.deterministic = True
-        
-        image_augmented, label_augmented = seq1(image=X,  bounding_boxes=Y)
-        #image_augmented[i] = seq2.augment_image(image_augmented[i])
-  
-        if show_image:
-            visualizeImageAndLabel(self.train_imgs, self.test_labels, image_augmented ,label_augmented)
 
     
         
--- model.py
+++ model.py
@@ -23,13 +23,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 from jsonParser import Parser
 from generator import Generator
-#from root_path + "generator.ipynb" import Generator
-# how to extract data
-#import zipfile
-#zip_ref = zipfile.ZipFile(root_path + "/synthetic.zip", 'r')
-#zip_ref.extractall("/tmp")
-#zip_ref.close()
-generateData = False
+
 # train data and labels
 directory_path = './synthetic/'
 data = listdir(directory_path)
@@ -41,8 +35,10 @@ print("schleife beginnt jetzt")
 for dataIdx in range (len(data)):
     if os.path.splitext(data[dataIdx])[1][1:] == "png":
         x_paths.append(directory_path + data[dataIdx])
+        
     elif os.path.splitext(data[dataIdx])[1][1:] == "json":
         y_paths.append(directory_path + data[dataIdx])
+        
 x_paths = np.array(x_paths)
 y_paths = np.array(y_paths)
 
@@ -86,7 +82,6 @@ for y_path in y_paths:
     y_3D_labelBox.append(parser.get_3D_data())
     y_centerPoint_Label.append(parser.get_centerPoint())
     
-
 # split data into training data and test data
 
 y_2D_labelBox = np.array(y_2D_labelBox)
@@ -98,15 +93,119 @@ y_2D_labelValData = y_2D_labelBox[splitTrainValData:]
 y_3D_labelTrainData = y_3D_labelBox[0:splitTrainValData]
 y_3D_labelValData =  y_3D_labelBox[splitTrainValData:]
 
-if generateData:
-    gen_2d = Generator(x_imgTrainData, y_2D_labelTrainData, x_imgValData, y_2D_labelValData) # return (x_img append new_img)
-    [x_imgTrainData, y_2D_labelTrainData, x_imgValData, y_2D_labelValData] = gen_2d.get_data_after_augment()
-#    gen3d = Generator(x_imgTrainData, y_3D_labelTrainData, x_imgValData, y_3D_labelValData) # return (x_img append new_img)
+def proceed_2D_training (useGen, callbacks):
+    global x_imgTrainData, y_2D_labelTrainData, x_imgValData, y_2D_labelValData
 
+    if useGen is True:
+        print("Starting 2D augmentation")
+        gen_2d = Generator(x_imgTrainData, y_2D_labelTrainData, x_imgValData, y_2D_labelValData) # return (x_img append new_img)
+        [x_imgTrainData, y_2D_labelTrainData, x_imgValData, y_2D_labelValData] = gen_2d.get_data_after_augment('2D')
+        
+    
+    print("Start building 2D model")
+    model2D = getCreated2DModel()
+    model2D.compile(optimizer="adam", loss='mean_squared_error', metrics=['mean_absolute_error'])
+    
+    print("starting the 2D training")
+    hist = model2D.fit(x_imgTrainData, y_2D_labelTrainData,
+                       validation_data = (x_imgValData, y_2D_labelValData), 
+                       batch_size=16, epochs=50, 
+                       callbacks=callbacks)
+  
+    return [model2D, hist]
+  
 
-print("parser durch")
+def proceed_3D_training (useGen, callbacks):
+    global x_imgTrainData, y_3D_labelTrainData, x_imgValData, y_3D_labelValData
+    
+    if useGen is True:
+        print("Starting 3D augmentation")
+        gen_3d = Generator(x_imgTrainData, y_3D_labelTrainData, x_imgValData, y_3D_labelValData) # return (x_img append new_img)
+        [x_imgTrainData, y_3D_labelTrainData, x_imgValData, y_3D_labelValData] = gen_3d.get_data_after_augment('3D')
+
+  
+    print("Start building 3D model")
+    model3D = getCreated3DModel()
+    model3D.compile(optimizer="adam", loss='mean_squared_error', metrics=['mean_absolute_error'])
+    
+    print("starting the 3D training")
+    hist = model3D.fit(x_imgTrainData, y_3D_labelTrainData,
+                       validation_data = (x_imgValData, y_3D_labelValData), 
+                       batch_size=16, epochs=10, 
+                       callbacks=callbacks)
+  
+    return [model3D, hist]
+  
+def predict_2D_coordinates(model):
+    
+    for i in range(100):
+        predImg = np.zeros(shape=(1,512,512,3))
+        img = cv2.resize(cv2.imread("./synthetic/000"+str(800+i)+".is.png"),(512,512))
+        predImg[0] = cv2.resize(img,(512,512))
+        
+        parser = Parser("./synthetic/000"+str(800+i)+".json")
+        #-------------------
+        predictedData = model.predict(predImg)
+        predictedData[predictedData < 0] = 0
+        
+        imgLabel = parser.get_2D_data()
+        
+        print("myData: ", imgLabel)
+        print("myPredictedData: ", predictedData, " ", len(predictedData))
+    
+    
+    import matplotlib.patches as patches
+    
+    fig,ax = plt.subplots(1)
+    
+    ax.imshow(img)
+    
+    xValue = predictedData.item(1)*512
+    yValue = predictedData.item(0)*512
+    width = predictedData.item(3)*512 - xValue
+    height = predictedData.item(2)*512 - yValue
+    
+    xValueLabel = imgLabel[1]*512
+    yValueLabel = imgLabel[0]*512
+    widthLabel = imgLabel[3]*512 - xValueLabel
+    heightLabel = imgLabel[2]*512 - yValueLabel
+    
+    rectLabel = patches.Rectangle((xValueLabel,yValueLabel),widthLabel,heightLabel,linewidth=1,edgecolor='b',facecolor='none')
+    ax.add_patch(rectLabel)
+    
+    rectPred = patches.Rectangle((xValue,yValue),width,height,linewidth=1,edgecolor='r',facecolor='none')
+    ax.add_patch(rectPred)
+    plt.show()
+    
+    
+def predict_3D_coordinates(model):
+    
+    for i in range(2):
+        predImg = np.zeros(shape=(1,512,512,3))
+        img = cv2.resize(cv2.imread("./synthetic/000"+str(800+i)+".is.png"),(512,512))
+        predImg[0] = cv2.resize(img,(512,512))
+        
+        parser = Parser("drive/My Drive/synthetic/000"+str(800+i)+".json")
+        #-------------------
+        predictedData = model.predict(predImg)
+        predictedData[predictedData < 0] = 0
+        
+        img3DLabel = parser.get_3D_data()
+        
+        print("myData: ", img3DLabel)
+        print("myPredictedData: ", predictedData, " ", len(predictedData))
+        print("----------------------------")
+        
+
+def show_hist(hist):
+    for cnt, key in enumerate(hist.history.keys()):
+        plt.figure(999 + cnt)
+        plt.plot(hist.history[key])
+        plt.title(key.replace("_"," "))
+    
+    
 def getCreated2DModel():
-  inputs = Input((512,512,3))   #evtl anpassen
+  inputs = Input((448,448,3))   #evtl anpassen
   
   conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
   pool1 = MaxPooling2D(pool_size=(2, 2), strides=2)(conv1)
@@ -198,110 +297,25 @@ def getCreated3DModel():
 
   return model
 
-model2D = getCreated2DModel()
-model3D = getCreated3DModel()
-print("vor compile")
-# falsche loss funktion
 #adam = Adam(learning_rate=0.0001, beta_1=0.9, beta_2=0.999, amsgrad=False)
-model2D.compile(optimizer="adam", loss='mean_squared_error', metrics=['mean_absolute_error'])
-model3D.compile(optimizer="adam", loss='mean_squared_error', metrics=['mean_absolute_error'])
-print("nach compile")
 
-print("vor fit")
 #keras.callbacks.callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=0, save_best_only=False, save_weights_only=False, mode='auto', period=1)
 #keras.callbacks.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=0, mode='auto', baseline=None, restore_best_weights=False)
 #keras.callbacks.tensorboard_v1.TensorBoard(log_dir='./logs', histogram_freq=0, batch_size=32, write_graph=True, write_grads=False, write_images=False, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None, embeddings_data=None, update_freq='epoch')
 filepath = "./model_"+(datetime.now().strftime("%Y_%m_%d_%H_%M_%S")).replace(" ","_")+"_epoch({epoch:02d})-val_loss({val_loss:.4f})-loss({loss:.4f}).hdf5"
-callbacks = [ModelCheckpoint(filepath, monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=False, mode='min', period=1)
+callbacks = [ModelCheckpoint(filepath, monitor='val_loss', verbose=0, save_best_only=True, save_weights_only=True, mode='min', period=1)
              ,EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=0, mode='min', baseline=None, restore_best_weights=False) # stops after 5 epochs if the val_loss doesnt decrease
-             ,TensorBoard(log_dir='.\\', histogram_freq=1, batch_size=16,profile_batch = 100000000, write_graph=True, write_grads=True, write_images=True, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None, embeddings_data=None, update_freq='epoch')
+            #  ,TensorBoard(log_dir='.\\', histogram_freq=1, batch_size=16,profile_batch = 100000000, write_graph=True, write_grads=True, write_images=True, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None, embeddings_data=None, update_freq='epoch')
              ,ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1, mode='min', min_delta=0.0001, cooldown=0, min_lr=0)
              ]
-#generator = Generator(x_pathsTrainData, y_2D_labelTrainData, 16)
-#generatorValidation = Generator(x_pathsValData, y_2D_labelValData, 16, validation=True)
+ 
 
-# 2D Model
-#hist = model2D.fit(x_imgTrainData, y_2D_labelTrainData, validation_data = (x_imgValData, y_2D_labelValData), batch_size=16, epochs=10, callbacks=callbacks)
-# 3D Model
-hist = model3D.fit(x_imgTrainData, y_3D_labelTrainData, validation_data = (x_imgValData, y_3D_labelValData), batch_size=16, epochs=50, callbacks=callbacks)
-
-
-#
-print("nach fit")
-#
-for cnt,key in enumerate(hist.history.keys()):
-    
-    plt.figure(999 + cnt)
-    plt.plot(hist.history[key])
-    plt.title(key.replace("_"," "))
-
-#for cnt, key in enumerate(histGenerator.history.keys()):
-#    plt.figure(999 + cnt)
-#    plt.plot(histGenerator.history[key])
-#    plt.title(key.replace("_"," "))
-
-# here print 2d Box predicted images
-    
-#for i in range(100):
-#        
-#    #-------------------
-#    predImg = np.zeros(shape=(1,512,512,3))
-#    img = cv2.resize(cv2.imread("./synthetic/000"+str(800+i)+".is.png"),(512,512))
-#    predImg[0] = cv2.resize(img,(512,512))
-#    
-#    parser = Parser("./synthetic/000"+str(800+i)+".json")
-#    #-------------------
-#    predictedData = model.predict(predImg)
-#    predictedData[predictedData < 0] = 0
-#    
-#    imgLabel = parser.get_2D_data()
-#    
-#    print("myData: ", imgLabel)
-#    print("myPredictedData: ", predictedData, " ", len(predictedData))
-#    
-#    
-#    import matplotlib.patches as patches
-#    
-#    fig,ax = plt.subplots(1)
-#    
-#    ax.imshow(img)
-#    
-#    xValue = predictedData.item(1)*512
-#    yValue = predictedData.item(0)*512
-#    width = predictedData.item(3)*512 - xValue
-#    height = predictedData.item(2)*512 - yValue
-#    
-#    xValueLabel = imgLabel[1]*512
-#    yValueLabel = imgLabel[0]*512
-#    widthLabel = imgLabel[3]*512 - xValueLabel
-#    heightLabel = imgLabel[2]*512 - yValueLabel
-#    
-#    rectLabel = patches.Rectangle((xValueLabel,yValueLabel),widthLabel,heightLabel,linewidth=1,edgecolor='b',facecolor='none')
-#    ax.add_patch(rectLabel)
-#    
-#    rectPred = patches.Rectangle((xValue,yValue),width,height,linewidth=1,edgecolor='r',facecolor='none')
-#    ax.add_patch(rectPred)
-#    plt.show()
+################### main ################################
+#[model_3d, hist_3d] = proceed_3D_training (True, callbacks)
+#show_hist(hist_3d)
+#predict_3D_coordinates(model_3d)
 
 
-
-# here print 3d Box predicted images
-    
-for i in range(2):
-        
-    #-------------------
-    predImg = np.zeros(shape=(1,512,512,3))
-    img = cv2.resize(cv2.imread("./synthetic/000"+str(800+i)+".is.png"),(512,512))
-    predImg[0] = cv2.resize(img,(512,512))
-    
-    parser = Parser("./synthetic/000"+str(800+i)+".json")
-    #-------------------
-    predictedData = model3D.predict(predImg)
-    predictedData[predictedData < 0] = 0
-    
-    img3DLabel = parser.get_3D_data()
-    
-    print("myData: ", img3DLabel)
-    print("myPredictedData: ", predictedData, " ", len(predictedData))
-    print("----------------------------")
-    
+[model_2d, hist_2d] = proceed_2D_training (True, callbacks)
+show_hist(hist_2d)
+predict_2D_coordinates(model_2d)
